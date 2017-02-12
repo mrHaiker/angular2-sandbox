d5b6ba8346de7578baf2cf56c245f9b24ec21420
Sergey <mr.haiker@mail.ru>
2017-02-12 15:11:20 +0200

    Перенес разработку в другую папку и на другую версию @angular/cli

---
+++ market-app/src/app/header-menu/header-menu.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header-menu',
+  templateUrl: './header-menu.component.html',
+  styleUrls: ['./header-menu.component.scss']
+})
+export class HeaderMenuComponent implements OnInit {
+  list: string[];
+
+  constructor() {
+    this.list = [
+      'Samsung',
+      'Apple',
+      'Meizu',
+      'LG',
+      'Xiomi',
+    ]
+  }
+
+  ngOnInit() {
+  }
+
+}
--- market/app/components/header/header-menu.component.ts
+++
@@ -1,1 +0,0 @@
-import {Component} from "@angular/core";@Component({    selector: "header-menu",    templateUrl: 'app/components/header/header-menu.component.html'})export class HeaderMenu {}
