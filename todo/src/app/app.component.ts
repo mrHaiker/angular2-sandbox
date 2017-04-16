4fb41d80490d3e7cd54b2d930a5ca451c9c935bb
Sergey <mr.haiker@mail.ru>
2017-04-16 14:56:07 +0300

    #Директивы

--- todo/src/app/app.component.ts
+++ todo/src/app/app.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 
+const todos = [
+  'Изучить JS',
+  'Изучить Angular 2',
+  'написать приложение'
+];
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +13,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular 2Do';
+  todos = todos;
 }
